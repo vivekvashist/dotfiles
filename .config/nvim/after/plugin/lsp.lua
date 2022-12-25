780a9e372141f8f9b6f2b633366959e6e6bdff15
Vivek <vivekvashist@gmail.com>
2022-12-25 19:14:04 +1100

    fixed lspconfig for sunmeko lua

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,4 +1,15 @@
 local lsp = require('lsp-zero')
+
 lsp.preset('recommended')
+
+
+-- Setup lua server for neovim
+require('lspconfig').sumneko_lua.setup(
+  lsp.build_options('sumneko_lua', lsp.defaults.nvim_workspace())
+  )
+-- Add neovim api source to nvim-cmp
+local cmp_sources = lsp.defaults.cmp_sources()
+table.insert(cmp_sources, {name = 'nvim_lua'})
+require('cmp').setup.filetype('lua', {sources = cmp_sources})
 
 lsp.setup()
